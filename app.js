580ba15cade152e11db5edcfea8c6449eec6a944
Sakharov, Alexey <alexei.saharov@gmail.com>
2018-05-04 16:46:00 +0300

    Implemented writing into file

--- app.js
+++ app.js
@@ -10,20 +10,40 @@ app.use(bodyParser.json());
 
 app.post("/questions", urlencodedParser, function (request, response) {
     if(!request.body) return response.sendStatus(400);
-    console.log(request.body);
   
-    var data = `${request.body.author} - ${request.body.text}`;
-    response.type('json');
-    response.send(data);
-    console.log(data);
+    var requestBody = request.body;
+
+    var questionsObj;
+    fs.readFile('questions.json', 'utf8', function (err, data) {
+        if (err) response.sendStatus(500);
+        questionsObj = JSON.parse(data);
+
+        var questionAnswers = questionsObj.question.answers;
+
+        var enrichedData = {
+            id : (questionAnswers[questionAnswers.length - 1].id + 1),
+            text : requestBody.text,
+            author: requestBody.author,
+            date: new Date(),
+            rate: Math.floor(Math.random() * (6 - 1)) + 1
+        };
+
+        questionsObj.question.answers.push(enrichedData);
+        
+        fs.writeFile('questions.json', JSON.stringify(questionsObj, null, 4), function(error) {
+            if (err) response.sendStatus(500);
+            response.type('json');
+            response.send(enrichedData);
+        });
+    });
 });
 
 app.get("/questions", function(request, response) {
-    var obj;
+    var questionsObj;
     fs.readFile('questions.json', 'utf8', function (err, data) {
-        if (err) throw err;
-        obj = JSON.parse(data);
-        response.send(obj);
+        if (err) response.sendStatus(500);
+        questionsObj = JSON.parse(data);
+        response.send(questionsObj);
     });
 });
 
